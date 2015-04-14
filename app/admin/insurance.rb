2cc1d2b894c0d8b0bbd6f8f0784e4f6206a3ce07
Prelang Builder <builder@prelang.com>
2015-04-14 22:15:36 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Insurance' to allow for create, update

--- app/admin/insurance.rb
+++ app/admin/insurance.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Insurance do
+  permit_params :last, :future
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
