4110d4ab4f91a31af9093cc06bf6ab21d3a8fe2a
Prelang Builder <builder@prelang.com>
2015-04-14 22:15:42 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Inspection' to allow for create, update

--- app/admin/inspection.rb
+++ app/admin/inspection.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Inspection do
+  permit_params :last, :future
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
