cd141fef35061d766486e4eb4974067bcd4a20ce
Prelang Builder <builder@prelang.com>
2015-04-14 22:15:49 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Route' to allow for create, update

--- app/admin/route.rb
+++ app/admin/route.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Route do
+  permit_params :start_date, :end_date, :from, :to, :length, :fuel_cost, :luggage, :finished, :accident
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
