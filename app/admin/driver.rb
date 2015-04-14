9f3baaacb2665bda29769907a2abe2df662dcb87
Prelang Builder <builder@prelang.com>
2015-04-14 22:15:55 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Driver' to allow for create, update

--- app/admin/driver.rb
+++ app/admin/driver.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Driver do
+  permit_params :name, :lastname, :pesel, :phone, :join_date, :category
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
