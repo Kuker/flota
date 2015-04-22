bc4702ea5ecdd94d182c4e3b36253b72f7d476be
Kuker <kuker16@gmail.com>
2015-04-22 22:43:11 +0200

    Zmiana z logowania przez email na login.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,18 +4,21 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :trackable, :validatable
-  attr_accessor :email
+  attr_accessor :login
   
   #->Prelang (user_login:devise/username_login_support)
-  def self.find_first_by_auth_conditions(warden_conditions)
+  def self.find_for_database_authentication(warden_conditions)
     conditions = warden_conditions.dup
     if login = conditions.delete(:login)
-      where(conditions).where(["lower(username) = :value OR lower(email) = :value", {value: login.downcase}]).first
+      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
     else
-      where(conditions).first
+      where(conditions.to_h).first
     end
   end
-
-
-  devise authentication_keys: [:email]
+  validates :username,
+            :presence => true,
+            :uniqueness => {
+                :case_sensitive => false
+            }
+  #devise authentication_keys: [:login]
 end
