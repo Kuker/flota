19a4c1388c6ceae1dacfe58d5a78339a4f2d0e90
Kuker <kuker16@gmail.com>
2015-05-20 20:01:01 +0200

    Validation when adding car

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,4 +1,5 @@
 class Car < ActiveRecord::Base
+  validates :plate_no, :brand, :model, :production, :fuel_type, :color, :fuel_consumption, :capacity, :category, :mileage, presence: true
   has_one :route
   has_one :inspection
   has_one :insurance
