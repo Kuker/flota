10071499caeb771b34282b7a757551d2e70e6f1b
Kuker <kuker16@gmail.com>
2015-05-20 19:16:32 +0200

    Validation when adding new route

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,4 +1,5 @@
 class Route < ActiveRecord::Base
+  validates :start_date, :end_date, :from, :to, :length, :fuel_cost, :luggage, :car_id, :driver_id, presence: true
   belongs_to :car
   belongs_to :driver
 end
