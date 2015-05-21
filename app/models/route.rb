8192e903dba61fee5818fa21698c0a8e6e97723c
Kuker <kuker16@gmail.com>
2015-05-21 17:33:09 +0200

    more changed to approx fuel cost in routes

--- app/models/route.rb
+++ app/models/route.rb
@@ -1,5 +1,5 @@
 class Route < ActiveRecord::Base
-  validates :start_date, :end_date, :from, :to, :length, :fuel_cost, :luggage, :car_id, :driver_id, presence: true
+  validates :start_date, :end_date, :from, :to, :length, :luggage, :car_id, :driver_id, presence: true
   belongs_to :car
   belongs_to :driver
 end
